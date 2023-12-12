236af0259087871c5e892ea39d689e128fe0c6d0
Ribbit <echo07200503@gmail.com>
2023-12-12 17:51:20 +0900

    ♻ Refactor event manager code

--- src/desktop/event.tsx
+++ src/desktop/event.tsx
@@ -9,75 +9,76 @@ import { PLUGIN_NAME } from '@/lib/static';
 import { manager } from '@/lib/event-manager';
 import { PLUGIN_ID } from '@/lib/global';
 
-const events: launcher.Events = [
-  'app.record.create.show',
-  'app.record.edit.show',
-  'app.record.detail.show',
-  'app.record.index.show',
-];
-
 let rendered = false;
 
-manager.add(events, (event) => {
-  if (rendered && event.type.includes('index')) {
+manager.add(
+  [
+    'app.record.create.show',
+    'app.record.edit.show',
+    'app.record.detail.show',
+    'app.record.index.show',
+  ],
+  (event) => {
+    if (rendered && event.type.includes('index')) {
+      return event;
+    }
+
+    const config = restoreStorage(PLUGIN_ID);
+    rendered = true;
+
+    const metaFields = getFields();
+
+    for (const condition of config.conditions) {
+      condition.field;
+
+      const metaField = metaFields.find((field) => field.var === condition.field);
+      if (!metaField) {
+        console.error(
+          `[${PLUGIN_NAME}] 設定したフィールドが見つからなかったため、処理を中断しました`
+        );
+        continue;
+      }
+
+      const target =
+        document.querySelector(`.label-${metaField.id} > div`) ||
+        document.querySelector(`.label-${metaField.id}`);
+      if (!target) {
+        console.error(
+          `[${PLUGIN_NAME}] 設定したフィールドが見つからなかったため、処理を中断しました`
+        );
+        continue;
+      }
+
+      target.classList.add(css`
+        display: flex;
+        align-items: center;
+        gap: 1rem;
+
+        * {
+          line-height: 1;
+        }
+      `);
+
+      const root = document.createElement('span');
+      target.append(root);
+      createRoot(root).render(
+        <Tooltip
+          title={
+            <>
+              {condition.label.split(/\n/).map((line, i) => (
+                <div key={i}>{line || '　'}</div>
+              ))}
+            </>
+          }
+          placement='top'
+        >
+          <span>
+            <HelpIcon fill='#999c' style={{ width: '1.1rem', height: '1.1rem' }} />
+          </span>
+        </Tooltip>
+      );
+    }
+
     return event;
   }
-
-  const config = restoreStorage(PLUGIN_ID);
-  rendered = true;
-
-  const metaFields = getFields();
-
-  for (const condition of config.conditions) {
-    condition.field;
-
-    const metaField = metaFields.find((field) => field.var === condition.field);
-    if (!metaField) {
-      console.error(
-        `[${PLUGIN_NAME}] 設定したフィールドが見つからなかったため、処理を中断しました`
-      );
-      continue;
-    }
-
-    const target =
-      document.querySelector(`.label-${metaField.id} > div`) ||
-      document.querySelector(`.label-${metaField.id}`);
-    if (!target) {
-      console.error(
-        `[${PLUGIN_NAME}] 設定したフィールドが見つからなかったため、処理を中断しました`
-      );
-      continue;
-    }
-
-    target.classList.add(css`
-      display: flex;
-      align-items: center;
-      gap: 1rem;
-
-      * {
-        line-height: 1;
-      }
-    `);
-
-    const root = document.createElement('span');
-    target.append(root);
-    createRoot(root).render(
-      <Tooltip
-        title={
-          <>
-            {condition.label.split(/\n/).map((line, i) => (
-              <div key={i}>{line || '　'}</div>
-            ))}
-          </>
-        }
-        placement='top'
-      >
-        <span>
-          <HelpIcon fill='#999c' style={{ width: '1.1rem', height: '1.1rem' }} />
-        </span>
-      </Tooltip>
-    );
-  }
-
-  return event;
-});
+);
