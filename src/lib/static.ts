a2df0436f54f96e0e3d5b5d81eddacf5acdde7df
Ribbit <echo07200503@gmail.com>
2023-12-12 17:51:46 +0900

    👍 Update import and URLs in static.ts

--- src/lib/static.ts
+++ src/lib/static.ts
@@ -1,9 +1,7 @@
-import manifest from '../../plugin/manifest.json';
+import config from '../../plugin.config.mjs';
 
-export const PLUGIN_NAME = manifest.name.ja;
+export const PLUGIN_NAME = config.manifest.base.name.ja;
 
-export const LOCAL_STORAGE_KEY = 'ribbit-kintone-plugin';
-
-export const URL_HOMEPAGE = manifest.homepage_url.ja;
 export const URL_INQUIRY = 'https://form.konomi.app';
 export const URL_PROMOTION = 'https://promotion.konomi.app/kintone-plugin';
+export const URL_BANNER = 'https://promotion.konomi.app/kintone-plugin/sidebar';
