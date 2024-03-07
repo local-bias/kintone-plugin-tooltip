a1b7a981260702d1795f358d26e9a5f39961849a
Ribbit <echo07200503@gmail.com>
2024-03-07 20:02:34 +0900

    👍 CDNを設定

--- plugin.config.mjs
+++ plugin.config.mjs
@@ -1,16 +1,17 @@
 //@ts-check
 const hp = 'https://konomi.app/';
-const commonCdn = 'https://kintone-plugin.konomi.app/common';
+const cdn = 'https://kintone-plugin.konomi.app';
+const key = 'tooltip';
 const localhost = 'https://127.0.0.1:3940';
 
 /** @type { import('@konomi-app/kintone-utilities').PluginConfig } */
 export default {
-  id: 'ribbit-kintone-plugin-tooltip',
+  id: `ribbit-kintone-plugin-${key}`,
   pluginReleasePageUrl: `https://ribbit.konomi.app/kintone-plugin/`,
   manifest: {
     base: {
       manifest_version: 1,
-      version: '2.0.1',
+      version: '2.1.0',
       type: 'APP',
       name: {
         en: 'Tool chip plugin',
@@ -23,40 +24,34 @@ export default {
         zh: '将图标添加到特定字段并在您将鼠标悬停在该字段上时显示提示',
       },
       icon: 'icon.png',
-      homepage_url: { ja: hp, en: hp, zh: hp },
-      desktop: {
-        js: ['https://cdn.jsdelivr.net/gh/local-bias/kintone-cdn@latest/dist/desktop.js'],
-        css: [],
-      },
-      mobile: {
-        js: ['https://cdn.jsdelivr.net/gh/local-bias/kintone-cdn@latest/dist/desktop.js'],
-        css: [],
-      },
+      homepage_url: { ja: hp, en: hp },
+      desktop: { js: [`${cdn}/common/desktop.js`], css: [] },
+      mobile: { js: [`${cdn}/common/desktop.js`], css: [] },
       config: {
         html: 'config.html',
-        js: ['https://cdn.jsdelivr.net/gh/local-bias/kintone-cdn@latest/dist/config.js'],
+        js: [`${cdn}/common/config.js`],
         css: [],
         required_params: [],
       },
     },
     dev: {
       desktop: {
-        js: [`${localhost}/dist/dev/desktop/index.js`],
+        js: [`${localhost}/dist/dev/desktop.js`],
         css: [`${localhost}/dist/dev/desktop.css`],
       },
       mobile: {
-        js: [`${localhost}/dist/dev/desktop/index.js`],
+        js: [`${localhost}/dist/dev/desktop.js`],
         css: [`${localhost}/dist/dev/desktop.css`],
       },
       config: {
-        js: [`${localhost}/dist/dev/config/index.js`],
+        js: [`${localhost}/dist/dev/config.js`],
         css: [`${localhost}/dist/dev/config.css`],
       },
     },
     prod: {
-      desktop: { js: ['desktop.js'], css: ['desktop.css'] },
-      mobile: { js: ['desktop.js'], css: ['desktop.css'] },
-      config: { js: ['config.js'], css: ['config.css'] },
+      desktop: { js: [`${cdn}/${key}/desktop.js`], css: [`${cdn}/${key}/desktop.css`] },
+      mobile: { js: [`${cdn}/${key}/desktop.js`], css: [`${cdn}/${key}/desktop.css`] },
+      config: { js: [`${cdn}/${key}/config.js`], css: [`${cdn}/${key}/config.css`] },
     },
     standalone: {
       desktop: { js: ['desktop.js'], css: ['desktop.css'] },
