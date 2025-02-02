3e947cc1479b00b9447df5f0da175f259056e591
Ribbit <echo07200503@gmail.com>
2025-02-02 16:14:24 +0900

    ♻ recoil ➡ jotai

--- src/config/components/footer/import-button.tsx
+++ src/config/components/footer/import-button.tsx
@@ -1,36 +1,34 @@
-import React, { ChangeEventHandler, FC, memo } from 'react';
-import { useRecoilCallback } from 'recoil';
-import { useSnackbar } from 'notistack';
+import { migrateConfig } from '@/lib/plugin';
 import { onFileLoad } from '@konomi-app/kintone-utilities';
 import { PluginConfigImportButton } from '@konomi-app/kintone-utilities-react';
-import { storageState } from '../../states/plugin';
-import { migrateConfig } from '@/lib/plugin';
+import { useAtomCallback } from 'jotai/utils';
+import { useSnackbar } from 'notistack';
+import { ChangeEventHandler, FC, memo, useCallback } from 'react';
+import { pluginConfigAtom } from '../../states/plugin';
 
 const Component: FC = () => {
   const { enqueueSnackbar } = useSnackbar();
 
-  const onChange: ChangeEventHandler<HTMLInputElement> = useRecoilCallback(
-    ({ set }) =>
-      async (event) => {
-        try {
-          const { files } = event.target;
-          if (!files?.length) {
-            return;
-          }
-          const [file] = Array.from(files);
-          const fileEvent = await onFileLoad(file);
-          const text = (fileEvent.target?.result ?? '') as string;
-          set(storageState, migrateConfig(JSON.parse(text)));
-          enqueueSnackbar('設定情報をインポートしました', { variant: 'success' });
-        } catch (error) {
-          enqueueSnackbar(
-            '設定情報のインポートに失敗しました、ファイルに誤りがないか確認してください',
-            { variant: 'error' }
-          );
-          throw error;
+  const onChange: ChangeEventHandler<HTMLInputElement> = useAtomCallback(
+    useCallback(async (_, set, event) => {
+      try {
+        const { files } = event.target;
+        if (!files?.length) {
+          return;
         }
-      },
-    []
+        const [file] = Array.from(files);
+        const fileEvent = await onFileLoad(file);
+        const text = (fileEvent.target?.result ?? '') as string;
+        set(pluginConfigAtom, migrateConfig(JSON.parse(text)));
+        enqueueSnackbar('設定情報をインポートしました', { variant: 'success' });
+      } catch (error) {
+        enqueueSnackbar(
+          '設定情報のインポートに失敗しました、ファイルに誤りがないか確認してください',
+          { variant: 'error' }
+        );
+        throw error;
+      }
+    }, [])
   );
 
   return <PluginConfigImportButton onImportButtonClick={onChange} loading={false} />;
