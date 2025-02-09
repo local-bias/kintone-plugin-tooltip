d6e0c030908e18ed43d55473ed5e975d74e695be
Ribbit <echo07200503@gmail.com>
2025-02-09 13:04:54 +0900

    ♻ RecoilからJotaiへの状態管理の移行

--- src/config/states/kintone.ts
+++ src/config/states/kintone.ts
@@ -1,26 +1,21 @@
-import { selector } from 'recoil';
-import { getFormFields, kintoneAPI } from '@konomi-app/kintone-utilities';
 import { GUEST_SPACE_ID } from '@/lib/global';
 import { getAppId } from '@/lib/kintone';
+import { getFormFields, kintoneAPI } from '@konomi-app/kintone-utilities';
+import { atom } from 'jotai';
 
-const PREFIX = 'kintone';
+export const appFieldsAtom = atom<Promise<kintoneAPI.FieldProperty[]>>(async () => {
+  const app = getAppId();
+  if (!app) {
+    throw new Error('アプリのフィールド情報が取得できませんでした');
+  }
 
-export const appFieldsState = selector<kintoneAPI.FieldProperty[]>({
-  key: `${PREFIX}appFieldsState`,
-  get: async () => {
-    const app = getAppId();
-    if (!app) {
-      throw new Error('アプリのフィールド情報が取得できませんでした');
-    }
+  const { properties } = await getFormFields({
+    app,
+    preview: true,
+    guestSpaceId: GUEST_SPACE_ID,
+  });
 
-    const { properties } = await getFormFields({
-      app,
-      preview: true,
-      guestSpaceId: GUEST_SPACE_ID,
-    });
+  const values = Object.values(properties);
 
-    const values = Object.values(properties);
-
-    return values.sort((a, b) => a.label.localeCompare(b.label, 'ja'));
-  },
+  return values.sort((a, b) => a.label.localeCompare(b.label, 'ja'));
 });
