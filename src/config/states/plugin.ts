caaf68795444792a08ea04e9f489793259035526
Ribbit <echo07200503@gmail.com>
2025-02-16 11:44:03 +0900

    ♻ プラグイン設定情報の型をzodで再定義

--- src/config/states/plugin.ts
+++ src/config/states/plugin.ts
@@ -1,16 +1,17 @@
 import { restorePluginConfig } from '@/lib/plugin';
+import { PluginCondition, PluginConfig } from '@/schema/plugin-config';
 import { produce } from 'immer';
 import { atom } from 'jotai';
 import { atomWithDefault } from 'jotai/utils';
 import { SetStateAction } from 'react';
 
-export const pluginConfigAtom = atom<Plugin.Config>(restorePluginConfig());
+export const pluginConfigAtom = atom<PluginConfig>(restorePluginConfig());
 
 export const loadingAtom = atom<boolean>(false);
 
 export const pluginConditionsAtom = atom(
   (get) => get(pluginConfigAtom).conditions,
-  (_, set, newValue: SetStateAction<Plugin.Condition[]>) => {
+  (_, set, newValue: SetStateAction<PluginCondition[]>) => {
     set(pluginConfigAtom, (current) =>
       produce(current, (draft) => {
         draft.conditions = typeof newValue === 'function' ? newValue(draft.conditions) : newValue;
@@ -30,7 +31,7 @@ export const selectedConditionAtom = atom(
     const selectedConditionId = get(selectedConditionIdAtom);
     return conditions.find((condition) => condition.id === selectedConditionId) ?? conditions[0]!;
   },
-  (get, set, newValue: SetStateAction<Plugin.Condition>) => {
+  (get, set, newValue: SetStateAction<PluginCondition>) => {
     const selectedConditionId = get(selectedConditionIdAtom);
     const conditions = get(pluginConditionsAtom);
     const index = conditions.findIndex((condition) => condition.id === selectedConditionId);
@@ -51,12 +52,12 @@ export const conditionsLengthAtom = atom<number>((get) => {
   return conditions.length;
 });
 
-export const getConditionPropertyAtom = <T extends keyof Plugin.Condition>(property: T) =>
+export const getConditionPropertyAtom = <T extends keyof PluginCondition>(property: T) =>
   atom(
     (get) => {
       return get(selectedConditionAtom)[property];
     },
-    (_, set, newValue: SetStateAction<Plugin.Condition[T]>) => {
+    (_, set, newValue: SetStateAction<PluginCondition[T]>) => {
       set(selectedConditionAtom, (condition) =>
         produce(condition, (draft) => {
           draft[property] = typeof newValue === 'function' ? newValue(draft[property]) : newValue;
